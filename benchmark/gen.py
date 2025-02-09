2abb939ddd931fea3eff7c162285057cebc309a1
Ивасенко Андрей Павлович <apivasenko@edu.hse.ru>
2025-02-09 21:16:24 +0300

    update gen

--- benchmark/gen.py
+++ benchmark/gen.py
@@ -4,20 +4,34 @@ zipf_param = 2
 num_elements = 20_000_000 
 noise_mean = 0  
 noise_std = 1  
-output_file = "distribution.txt" 
 num_proportion = 0.7
 num_zipf_elements = int(num_elements * num_proportion)
 num_noise_elements = num_elements - num_zipf_elements
+parameters =[
+    [1.5,20_000_000,0,1,0.7],
+    [2,20_000_000,0,1,0.7],
+    [2,30_000_000,0,1,0.8],
+    [3.5,30_000_000,0,1,0.7],
+    [2,20_000_000,0,1,0.6]
+]
+for i, param in enumerate(parameters, start=0):
+    output_file = f"test_distribution_{i}.txt"
+    zipf_param = parameters[i][0]
+    num_elements = parameters[i][1]
+    noise_mean = parameters[i][2] 
+    noise_std = parameters[i][3]
+    num_proportion = parameters[i][4]
+    num_zipf_elements = int(num_elements * num_proportion)
+    num_noise_elements = num_elements - num_zipf_elements
+    zipf_data = np.random.zipf(zipf_param, num_zipf_elements)
+    
+    white_noise = np.random.normal(noise_mean, noise_std, num_noise_elements)
 
-zipf_data = np.random.zipf(zipf_param, num_zipf_elements)
 
-white_noise = np.random.normal(noise_mean, noise_std, num_noise_elements)
+    combined_data = np.concatenate((zipf_data, white_noise))
 
+    sorted_data = np.sort(combined_data)
 
-combined_data = np.concatenate((zipf_data, white_noise))
-
-sorted_data = np.sort(combined_data)
-
-with open(output_file, "w") as f:
-    for value in sorted_data:
-        f.write(f"{value}\n")
+    with open(output_file, "w") as f:
+        for value in sorted_data:
+            f.write(f"{value}\n")
--- benchmark/main.go
+++ benchmark/main.go
@@ -5,52 +5,61 @@ import (
 	"os"
 	"strconv"
 	"strings"
+
 	"github.com/influxdata/tdigest"
 )
 
 var quantiles = []float64{0.25, 0.5, 0.75, 0.90, 0.95, 0.99, 0.999}
 
 func main() {
-	cdf, err := LoadCdfImpl("distribution.txt")
-	if err != nil {
-		panic(err)
-	}
-	td := tdigest.New()
-	file, err := os.Open("distribution.txt")
-	if err != nil {
-		panic(err)
-	}
-	defer file.Close()
-
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		line := scanner.Text()
-		value, err := strconv.ParseFloat(strings.TrimSpace(line), 64)
+	for i := 0; i < 5; i++ {
+		inputFile := "test_distribution_" + strconv.Itoa(i) + ".txt"
+		outputFile := "result_" + strconv.Itoa(i) + ".txt"
+		cdf, err := LoadCdfImpl(inputFile)
 		if err != nil {
 			panic(err)
 		}
-		td.Add(value, 1)
-	}
 
-	if err := scanner.Err(); err != nil {
-		panic(err)
-	}
+		td := tdigest.New()
 
-	results := CompareCDF(cdf, td, quantiles)
-
-	outputFile, err := os.Create("result.txt")
-	if err != nil {
-		panic(err)
-	}
-	defer outputFile.Close()
-
-	writer := bufio.NewWriter(outputFile)
-	for _, result := range results {
-		_, err := writer.WriteString(result + "\n")
+		file, err := os.Open(inputFile)
 		if err != nil {
 			panic(err)
 		}
-	}
-	writer.Flush()
+		defer file.Close()
 
+
+		outFile, err := os.Create(outputFile)
+		if err != nil {
+			panic(err)
+		}
+		defer outFile.Close()
+
+		writer := bufio.NewWriter(outFile)
+
+		scanner := bufio.NewScanner(file)
+
+		for scanner.Scan() {
+			line := scanner.Text()
+			value, err := strconv.ParseFloat(strings.TrimSpace(line), 64)
+			if err != nil {
+				panic(err)
+			}
+			td.Add(value, 1)
+		}
+
+		if err := scanner.Err(); err != nil {
+			panic(err)
+		}
+
+		results := CompareCDF(cdf, td, quantiles)
+		for _, result := range results {
+			_, err := writer.WriteString(result + "\n")
+			if err != nil {
+				panic(err)
+			}
+		}
+
+		writer.Flush()
+	}
 }
